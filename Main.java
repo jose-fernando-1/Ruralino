fbf2dde56d61b52a69b7bcdeca331280fda2e06c
jose-fernando-1 <107815035+jose-fernando-1@users.noreply.github.com>
2023-09-07 16:33:01 -0300

    testando main

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+
         Pessoa pessoa1,pessoa2,pessoa3,pessoa4;
         List<Presente> listap1,listap2,listap3,listap4;
 
@@ -38,6 +39,52 @@ public class Main {
 
             System.out.println(doador.getNomeCompleto() + " -> " + recebedor.getNomeCompleto());
         }
+
+        System.out.print("\n\n\n\n");
+
+
+//*************************************************************************************
+
+
+
+        Pessoa pessoa5,pessoa6,pessoa7,pessoa8,pessoa9;
+        List<Presente> listap5,listap6,listap7,listap8,listap9;
+
+        listap5 = new ArrayList<>();
+        listap6 = new ArrayList<>();
+        listap7 = new ArrayList<>();
+        listap8 = new ArrayList<>();
+        listap9 = new ArrayList<>();
+
+        pessoa5 = new Pessoa("Marco aurelio", "marquinhos", "111", listap5);
+        pessoa6 = new Pessoa("Paulo Azevedo", "Paulo", "222", listap6);
+        pessoa7 = new Pessoa("Arthur Shelby", "tutu", "333", listap7);
+        pessoa8 = new Pessoa("Charles Chaplin", "Charles", "444", listap8);
+        pessoa9 = new Pessoa("Eduardo leao", "duda", "555", listap9);
+
+        List<Pessoa> listinha2 = new ArrayList<>();
+
+        listinha2.add(pessoa5);
+        listinha2.add(pessoa6);
+        listinha2.add(pessoa7);
+        listinha2.add(pessoa8);
+        listinha2.add(pessoa9);
+
+        Date data2 = new Date();
+        List<AmigoSorteado> amigoSorteadoslista2 = new ArrayList<>();
+        Grupo grupo2 = new Grupo("capeteemo", data2, listinha2, amigoSorteadoslista2);
+
+        List<AmigoSorteado> b = RepositorioPessoas.getInstanceRepositorioPessoas().sortear(grupo2);
+
+        for (AmigoSorteado amigoSorteado : b) {
+            Pessoa doador = amigoSorteado.getDoador();
+            Pessoa recebedor = amigoSorteado.getRecebedor();
+
+            System.out.println(doador.getNomeCompleto() + " -> " + recebedor.getNomeCompleto());
+        }
+
+
+
 
 
     }
